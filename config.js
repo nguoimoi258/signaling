ec0add88d8c0da77b5c1bdd0cca646fcdd8acc3b
bao <bao.buivan3@gmail.com>
2018-10-27 00:05:35 +0700

    initial project

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {};
+
+config.JWT_SECRETKEY = 'hu3/zldvRTAxpZhl/00xww9ZTBvlUHW8eKeQ8xvRR1Y=';
+config.PORT = 8453;
+config.SIGNALING_URL = 'wss://io.gtracking.net:8453';
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,52 @@
+var replify = require('replify');
+//var jwt = require('jsonwebtoken');
+var config = require('./config');
+var https = require('https');
+
+var privateKey  = fs.readFileSync('sslcert/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('sslcert/fullchain.pem', 'utf8');
+ 
+var credentials = {key: privateKey, 
+                    cert: certificate};
+
+var server = https.createServer(credentials);
+var switchboard = require('./')(server, { servelib: true });
+var port = parseInt(process.env.NODE_PORT || process.env.PORT || process.argv[2], 10) || config.PORT;
+var host = process.env.NODE_HOST || process.env.HOST || 'localhost';
+
+
+server.on('request', function(req, res) {
+  res.writeHead(200);
+  res.end('Wellcome to troidat.com\n');
+});
+
+// start the server
+server.listen(port, host, function(err) {
+  if (err) {
+    return console.log('Encountered error starting server: ', err);
+  }
+
+  console.log('server running at http://' + host + ':' + port + '/');
+});
+
+// add the repl
+replify({
+  name: 'switchboard',
+  app: switchboard,
+  contexts: {
+    server: server
+  }
+});
+
+switchboard.on('room:create', function(room) {
+  console.log('room ' + room + ' created, now have ' + switchboard.rooms.length + ' active rooms');
+});
+
+switchboard.on('room:destroy', function(room) {
+  console.log('room ' + room + ' destroyed, ' + switchboard.rooms.length + ' active rooms remain');
+
+  if (typeof gc == 'function') {
+    console.log('gc');
+    gc();
+  }
+});
